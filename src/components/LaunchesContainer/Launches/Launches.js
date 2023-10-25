0022a0f3273c800cb060105e861b880de88ad19d
GrayHead <ss.zhuravlov@gmail.com>
2023-10-25 14:49:18 +0300

    add homework lesson 2

--- src/components/LaunchesContainer/Launches/Launches.js
+++ src/components/LaunchesContainer/Launches/Launches.js
@@ -3,21 +3,20 @@ import {launchService} from "../../../services/launchService";
 import {Launch} from "../Launch/Launch";
 
 const Launches = () => {
-    const [launches, setLaunches]=useState([]);
+    const [launches, setLaunches] = useState([]);
 
 
-
-    useEffect(()=>{
-launchService.getAll().then(({data})=>{
-    const filter= data.filter(value=>value.launch_year!=="2020");
-    setLaunches(filter)
-})
-    },[])
+    useEffect(() => {
+        launchService.getAll().then(({data}) => {
+            const filter = data.filter(value => value.launch_year !== "2020");
+            setLaunches(filter)
+        })
+    }, [])
 
 
     return (
         <div>
-            {launches.map(launch=><Launch key={launch.launce_data_unix} launch={launch}/>)};
+            {launches.map(launch => <Launch key={launch.launce_data_unix} launch={launch}/>)};
         </div>
     );
 };
