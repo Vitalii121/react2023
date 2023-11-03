e1e1c595aeddece4608978e28f88ba5d0e1b9dab
GrayHead <ss.zhuravlov@gmail.com>
2023-11-03 10:49:41 +0200

    add homework lesson 5

---
+++ src/components/UsersContainer/UserDetails.js
@@ -0,0 +1,22 @@
+
+import {useNavigate} from "react-router-dom";
+
+const UserDetails = ({user}) => {
+    const {id, name, username, email, address: {street,city}} = user;
+
+    const navigate = useNavigate();
+
+    return (
+        <div>
+            <div>id: {id}</div>
+            <div>name: {name}</div>
+            <div>username: {username}</div>
+            <div>email: {email}</div>
+            <div>street: {street}</div>
+            <div>city: {city}</div>
+            <button onClick={() => navigate('posts')}>Get Posts</button>
+        </div>
+    );
+};
+
+export {UserDetails};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
+import {RouterProvider} from "react-router-dom";
+
 import reportWebVitals from './reportWebVitals';
+import {router} from "./router";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    <App />
+<RouterProvider router={router}/>
 
 );
 
