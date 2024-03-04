77674f5a9dc85ccb4f95f4730679df3676634ea2
GrayHead <ss.zhuravlov@gmail.com>
2024-03-04 21:41:11 +0200

    add mini proj

---
+++ src/pages/MovieDetailsPage.js
@@ -0,0 +1,11 @@
+import {MovieDetails} from "../components/MovieContainer/MovieDetails";
+
+const MovieDetailsPage = () => {
+    return (
+        <div>
+            <MovieDetails/>
+        </div>
+    );
+};
+
+export {MovieDetailsPage};
--- src/router.js
+++ src/router.js
@@ -2,12 +2,13 @@ import {createBrowserRouter} from "react-router-dom";
 
 import {MainLayout} from "./layouts";
 import {GenrePage, MoviesPage} from "./pages";
+import {MovieDetailsPage} from "./pages/MovieDetailsPage";
 
 const router = createBrowserRouter([
     {
         path:'', element:<MainLayout/>,children:[
             {path:'/discover/movie', element:<MoviesPage/>},
-
+            {path:'/discover/movie/:id', element:<MovieDetailsPage/>},
             {path:'/genre/movie/list', element:<GenrePage/>}
         ]
     }
